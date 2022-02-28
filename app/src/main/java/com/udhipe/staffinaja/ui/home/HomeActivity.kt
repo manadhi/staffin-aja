d3e640ee6e28562d27ea1ac1ba020416b7f51352
manadhi <udhi.permana@gmail.com>
2022-02-28 17:02:10 +0700

    feature:sarch or filter data

--- app/src/main/java/com/udhipe/staffinaja/ui/home/HomeActivity.kt
+++ app/src/main/java/com/udhipe/staffinaja/ui/home/HomeActivity.kt
@@ -35,9 +35,7 @@ class HomeActivity : AppCompatActivity() {
         homeViewModel.apply {
             getCandidateBlogList()
             combinedCandidateBlog.observe(this@HomeActivity, {
-                if (it.size > 0) {
-                    showData(it)
-                }
+                showData(it)
             })
         }
     }
@@ -49,11 +47,17 @@ class HomeActivity : AppCompatActivity() {
     private fun setListener() {
         binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
-                TODO("Not yet implemented")
+                // not used
+                return false
             }
 
-            override fun onQueryTextChange(newText: String?): Boolean {
-                TODO("Not yet implemented")
+            override fun onQueryTextChange(keyword: String?): Boolean {
+                if (keyword!!.isEmpty()) {
+                    homeViewModel.filterCandidateBlogList("")
+                } else if (keyword.length >= 2) {
+                    homeViewModel.filterCandidateBlogList(keyword)
+                }
+                return true
             }
 
         })
@@ -79,94 +83,5 @@ class HomeActivity : AppCompatActivity() {
             LinearLayoutManager(this@HomeActivity, LinearLayoutManager.VERTICAL, false)
         binding.rvCandidateBlog.adapter = adapter
         binding.rvCandidateBlog.layoutManager = linearLayoutManager
-
-        // dummy data
-/*        val dummyDataList = mutableListOf<PresenterModel>()
-
-        dummyDataList.add(
-            PresenterModel.Candidate(
-                0,
-                "Jaka Sembung",
-                "https://images.unsplash.com/photo-1590505695083-a6ceb62cc40e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTk4fHxodW1hbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-                20,
-                "Male",
-                false
-            )
-        )
-
-        dummyDataList.add(
-            PresenterModel.Candidate(
-                0,
-                "Jaka Sembung",
-                "https://images.unsplash.com/photo-1590505695083-a6ceb62cc40e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTk4fHxodW1hbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-                20,
-                "Male",
-                false
-            )
-        )
-
-        dummyDataList.add(
-            PresenterModel.Blog(
-                0, "How Handle time when Work from home",
-                "Working from home every day may have felt like a novelty six months ago, but for many of us, we’re in it for the long-haul now. Tech companies from Microsoft to Fujitsu are now giving employees the option to work from home permanently.",
-                "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1172&q=80",
-                "26 February 2022", "tag: work, wfh, pandemic"
-            )
-        )
-
-        dummyDataList.add(
-            PresenterModel.Candidate(
-                0,
-                "Jaka Sembung",
-                "https://images.unsplash.com/photo-1590505695083-a6ceb62cc40e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTk4fHxodW1hbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-                20,
-                "Male",
-                false
-            )
-        )
-
-        dummyDataList.add(
-            PresenterModel.Blog(
-                1, "How Handle time when Work from home",
-                "Working from home every day may have felt like a novelty six months ago, but for many of us, we’re in it for the long-haul now. Tech companies from Microsoft to Fujitsu are now giving employees the option to work from home permanently.",
-                "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1172&q=80",
-                "26 February 2022", "tag: work, wfh, pandemic"
-            )
-        )
-
-        dummyDataList.add(
-            PresenterModel.Candidate(
-                0,
-                "Jaka Sembung",
-                "https://images.unsplash.com/photo-1590505695083-a6ceb62cc40e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTk4fHxodW1hbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-                20,
-                "Male",
-                false
-            )
-        )
-
-        dummyDataList.add(
-            PresenterModel.Candidate(
-                0,
-                "Jaka Sembung",
-                "https://images.unsplash.com/photo-1590505695083-a6ceb62cc40e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTk4fHxodW1hbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-                20,
-                "Male",
-                false
-            )
-        )
-
-        dummyDataList.add(
-            PresenterModel.Candidate(
-                0,
-                "Jaka Sembung",
-                "https://images.unsplash.com/photo-1590505695083-a6ceb62cc40e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTk4fHxodW1hbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-                20,
-                "Male",
-                false
-            )
-        )
-
-        adapter.setData(dummyDataList)*/
     }
 }
--- app/src/main/java/com/udhipe/staffinaja/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/udhipe/staffinaja/ui/home/HomeViewModel.kt
@@ -11,41 +11,61 @@ class HomeViewModel(
     private val candidateUseCase: CandidateUseCase,
     private val blogUseCase: BlogUseCase
 ) : ViewModel() {
-//    private val mCandidateList = MutableLiveData<List<PresenterModel.Candidate>>()
-//    private val mBlogList = MutableLiveData<List<PresenterModel.Blog>>()
-//    private val presenterCandidateList = ArrayList<PresenterModel.Candidate>()
-//    private val presenterBlogList = ArrayList<PresenterModel.Blog>()
-//    private val presenterModelList = ArrayList<PresenterModel>()
-
 
     val combinedCandidateBlog: LiveData<MutableList<PresenterModel>>
         get() = mCombinedCandidateBlog
 
+    // for keeping original data
+    private val originalCandidateBlogList = mutableListOf<PresenterModel>()
+
+    // for expose data to view
     private val mCombinedCandidateBlog = MutableLiveData<MutableList<PresenterModel>>()
 
     init {
         mCombinedCandidateBlog.value = ArrayList()
     }
 
+    fun getCandidateBlogList() {
+        originalCandidateBlogList.clear()
+        mCombinedCandidateBlog.value?.clear()
+        getCandidateList()
+        getBlogList()
+    }
+
+    fun filterCandidateBlogList(keyword: String) {
+        if (keyword == "") {
+            mCombinedCandidateBlog.value?.clear()
+            mCombinedCandidateBlog.value = originalCandidateBlogList
+        } else {
+            val filteredCandidateBlogList = mutableListOf<PresenterModel>()
+            for (presenterModel: PresenterModel in originalCandidateBlogList) {
+                val wordToFilter = when (presenterModel) {
+                    is PresenterModel.Blog -> presenterModel.title
+                    is PresenterModel.Candidate -> presenterModel.name
+                }
+
+                if (wordToFilter.contains(keyword, ignoreCase = true)) {
+                    filteredCandidateBlogList.add(presenterModel)
+                }
+            }
+
+            mCombinedCandidateBlog.value?.clear()
+            mCombinedCandidateBlog.value = filteredCandidateBlogList
+        }
+    }
+
     private fun getCandidateList() {
         viewModelScope.launch {
             try {
                 val candidateList = candidateUseCase.getAllCandidate()
-
-//                presenterCandidateList.addAll(CandidateDataMapper.mapCandidateDomainModelToPresenter(candidateList))
-//                presenterModelList.addAll(presenterCandidateList)
-//                mCandidateList.value =
-//                    CandidateDataMapper.mapCandidateDomainModelToPresenter(candidateList)
-                mCombinedCandidateBlog.value?.addAll(
-                    CandidateDataMapper.mapCandidateDomainModelToPresenter(
-                        candidateList
-                    )
-                )
+                val candidatePresenterModelList =
+                    CandidateDataMapper.mapCandidateDomainModelToPresenter(candidateList)
+                originalCandidateBlogList.addAll(candidatePresenterModelList)
+                mCombinedCandidateBlog.value?.addAll(candidatePresenterModelList)
                 mCombinedCandidateBlog.value = mCombinedCandidateBlog.value
             } catch (exception: Exception) {
                 val message = exception.message
                 print(message)
-//                mCandidateList.value = arrayListOf()
             }
         }
     }
@@ -54,27 +74,14 @@ class HomeViewModel(
         viewModelScope.launch {
             try {
                 val blogList = blogUseCase.getAllBlog()
-//                presenterBlogList.addAll(CandidateDataMapper.mapBlogDomainModelToPresenter(blogList))
-//                presenterModelList.addAll(presenterBlogList)
-//                mBlogList.value = CandidateDataMapper.mapBlogDomainModelToPresenter(blogList)
-
-                mCombinedCandidateBlog.value?.addAll(
-                    BlogDataMapper.mapBlogDomainModelToPresenter(
-                        blogList
-                    )
-                )
+                val blogPresenterModelList = BlogDataMapper.mapBlogDomainModelToPresenter(blogList)
+                originalCandidateBlogList.addAll(blogPresenterModelList)
+                mCombinedCandidateBlog.value?.addAll(blogPresenterModelList)
                 mCombinedCandidateBlog.value = mCombinedCandidateBlog.value
             } catch (exception: Exception) {
                 val message = exception.message
                 print(message)
-//                mBlogList.value = arrayListOf()
             }
         }
-    }
-
-    fun getCandidateBlogList() {
-        mCombinedCandidateBlog.value?.clear()
-        getCandidateList()
-        getBlogList()
     }
 }
