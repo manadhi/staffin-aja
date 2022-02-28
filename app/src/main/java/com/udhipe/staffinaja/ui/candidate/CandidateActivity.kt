c06eb5a0e57061c85aafb2c63273cfeacaec9920
manadhi <udhi.permana@gmail.com>
2022-02-28 20:56:19 +0700

    feature:show candidate p1

--- app/src/main/java/com/udhipe/staffinaja/ui/candidate/CandidateActivity.kt
+++ app/src/main/java/com/udhipe/staffinaja/ui/candidate/CandidateActivity.kt
@@ -3,10 +3,13 @@ package com.udhipe.staffinaja.ui.candidate
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.udhipe.staffinaja.R
 import com.udhipe.staffinaja.databinding.ActivityCandidateBinding
 import com.udhipe.staffinaja.databinding.ActivityHomeBinding
+import com.udhipe.staffinaja.ui.common.PresenterModel
 import com.udhipe.staffinaja.ui.home.HomeViewModel
+import com.udhipe.staffinaja.util.GlideManager
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class CandidateActivity : AppCompatActivity() {
@@ -16,22 +19,38 @@ class CandidateActivity : AppCompatActivity() {
     }
 
     private lateinit var binding: ActivityCandidateBinding
-//    private val candidateViewModel: CandidateViewModel by viewModel()
+    private val candidateViewModel: CandidateViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCandidateBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        setView()
+        setView()
 //        setListener()
     }
 
-//    private fun setView() {
-//        TODO("Not yet implemented")
-//    }
-//
+    private fun setView() {
+        val candidatePresenterModel =
+            intent.getParcelableExtra<PresenterModel.Candidate>(CANDIDATE_DATA)
+        if (candidatePresenterModel != null) {
+            binding.tvCandidateId.text = getString(R.string.candidate_id, candidatePresenterModel.id)
+            GlideManager.loadImageResource(this@CandidateActivity, candidatePresenterModel.image, binding.imgCandidate)
+            if (candidatePresenterModel.expired) {
+                binding.tvCandidateExpired.visibility = View.VISIBLE
+            } else {
+                binding.tvCandidateExpired.visibility = View.GONE
+            }
+            binding.tvCandidateName.text = candidatePresenterModel.name
+            binding.tvCandidateAge.text = getString(R.string.candidate_age, candidatePresenterModel.age)
+            binding.tvCandidateGender.text = candidatePresenterModel.gender
+        }
+
+
+        val candidateId = intent.getIntExtra(CANDIDATE_ID, -1)
+    }
+
 //    private fun setListener() {
-//        TODO("Not yet implemented")
+//
 //    }
 }
